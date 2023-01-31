9a756c120d24d90582f253ca34374a856e9dce23
Jeremiah Hsing <jeremiah.hsing@gmail.com>
2023-01-31 17:16:02 +1100

    fix import sort bugs

--- src/components/Sections/Contact/ContactForm.tsx
+++ src/components/Sections/Contact/ContactForm.tsx
@@ -1,5 +1,5 @@
-import {FC, memo, useCallback, useMemo, useState} from 'react';
 import emailjs from '@emailjs/browser';
+import {FC, memo, useCallback, useMemo, useState} from 'react';
 
 
 interface FormData {
